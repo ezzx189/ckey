60f63805061887d14787aaedb62affa071d36c4e
ezzx189 <118227437+ezzx189@users.noreply.github.com>
2025-08-29 00:44:46 -0300

    Delete adswin.php

--- adswin.php
+++
@@ -1,8 +0,0 @@
-<?php
-// URL del archivo m3u8
-$url_m3u8 = "https://edge-live11-hr.cvattv.com.ar/live/c3eds/Adult_Swim/SA_Live_dash_enc/Adult_Swim.mpd";
-
-// Redirige a la URL del archivo m3u8
-header("Location: $url_m3u8");
-exit();
-?>
